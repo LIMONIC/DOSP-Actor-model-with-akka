2785bdff17d87d63e7a1454631ef368aa78dc47f
Tianyu <ztyame@gmail.com>
2021-09-17 09:11:06 -0400

    style: add comments

--- Program.fs
+++ Program.fs
@@ -2,17 +2,24 @@ open System
 open System.Security.Cryptography
 open System.Text
 
-let stripv3 (stripChars:string) (text:string) =
+/// Remove certain Character from string
+let removeChar (stripChars:string) (text:string) =
     text.Split(stripChars.ToCharArray(), StringSplitOptions.RemoveEmptyEntries) |> String.Concat
 
+/// Generate SHA256 of input string
 let generateSHA256Str (inputText: string) = 
     inputText
     |> Encoding.ASCII.GetBytes
     |> (new SHA256Managed()).ComputeHash
     |> System.BitConverter.ToString
-    |> stripv3 "-"
+    |> removeChar "-"
 
-
+/// count heading zeros of input string
+/// Use recursive
+/// Use parrten matcher for conditional judgement
+/// [] -> Recursive exit: when str become empty, return result
+/// h::t -> seperate a string to head and tail parts
+///     for example: a string "abcd" can be seperated into "a" and "bcd"
 let rec countHeadingZeros str cnt =  
     match str with
         | [] -> cnt
@@ -21,9 +28,12 @@ let rec countHeadingZeros str cnt =
             countHeadingZeros t cnt+1
           else cnt
 
+/// check if an input string has target number of zeros
+/// return 1 if true; 0 if false 
 let validateSHAStr str target = 
     let num = countHeadingZeros (str |> Seq.toList) 0
     if num = target then 1 else 0
+
 
 // Test: print out SHA256 String
 let str = "7wLfA2pSBgg2e6A"
