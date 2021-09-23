9582693e540731c93054078f3e31a8916f074b31
HenryLiu96 <71891241+HenryLiu96@users.noreply.github.com>
2021-09-23 16:48:20 -0400

    Update LocalActor.fsx

--- ActorModel/LocalActor.fsx
+++ ActorModel/LocalActor.fsx
@@ -1,29 +1,24 @@
+//LocalActor.fsx
 #r "nuget: Akka.FSharp" 
 #r "nuget: Akka.Remote"
 open System
 open Akka.Actor
 open Akka.Configuration
 open Akka.FSharp
+open Akka.Remote
 let configuration = 
     ConfigurationFactory.ParseString(
         @"akka {
-            actor {
-                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
-                deployment {
-                    /remoteecho {
-                        remote = ""akka.tcp://RemoteFSharp@127.0.0.1:9001""
-                    }
-                }
-            }
+            actor.provider = remote
             remote {
-                helios.tcp {
-                    port = 0
+                dot-netty.tcp {
+                    port = ""10010""
                     hostname = ""127.0.0.1""
                 }
             }
         }")
 let system = ActorSystem.Create("RemoteFSharp", configuration)
 let echoClient = system.ActorSelection("akka.tcp://RemoteFSharp@127.0.0.1:9001/user/EchoServer")
-let task = echoClient <? "F#!"
-let response : obj = Async.RunSynchronously (task, 1000)
+let task = echoClient <? "hello echo"
+let response :obj = Async.RunSynchronously (task, 1000)
 printfn "Reply from remote %s" (string(response))
