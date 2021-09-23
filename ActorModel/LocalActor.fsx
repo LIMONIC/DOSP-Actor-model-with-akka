45f355e448d22dfaa19cf890f924b76bdace1b85
Tianyu <ztyame@gmail.com>
2021-09-23 02:01:16 -0400

    feat: add local and remote actor model.

--- ActorModel/LocalActor.fsx
+++ ActorModel/LocalActor.fsx
@@ -1,4 +1,4 @@
-﻿#time "on"
+#time "on"
 #load "./Utils.fsx"
 #r "nuget: Akka.FSharp"
 #r "nuget: Akka.Remote"
@@ -28,7 +28,7 @@ let configuration =
             }
             remote {
                 helios.tcp {
-                    port = 0
+                    port = 8778
                     hostname = ""127.0.0.1""
                 }
             }
@@ -44,45 +44,51 @@ type Information =
     | Register of (string)
     | Done of (string)
 
-(*/ Print results and send them to server /*)
-let postMan (mailbox:Actor<_>) =
-    let mutable res = []
-    let remoteServer = system.ActorSelection("akka.tcp://StringDigger@127.0.0.1:8778/user/localActor")
-    // let sender = mailbox.Sender()
+let receiver (mailbox:Actor<_>) =
+    printfn "receiver on!"
     let rec loop () = actor {
-        let! message = mailbox.Receive()
-        // printfn "worker acotr receive msg: %A" message
-        let sender = mailbox.Sender()
-        let printAndSend resList = 
-            printfn "-------------RESULT-------------" 
-            resList |> List.iter(fun (str, sha256) -> printfn $"{str}\t{sha256}")
-            remoteServer <! Output(resList)
-            res <- []
-            printfn "--------------------------------" 
+        let! (message) = mailbox.Receive()
+        // let sender = mailbox.Sender()
+        // printfn $"Remote actor registired! info: {message}"
+        // sender <! sprintf "Echo: received"
+        // if (message :? string) then
+        //     printfn $"Remote actor registired! info: {message}"
+        // sender <! sprintf "Echo: %A" message
         match message with
-        | Register (info) -> 
-            // printfn $"Remote: send register {info}"
-            let response =  (Async.RunSynchronously (remoteServer <? Register(info)))
-            if response = (Register "Acknowledged") then 
-                sender <! ("Acknowledged")
-                printfn "[INFO] Registration Success!"
-            else
-                failwith "[ERROR] Unsuccessful registration!"
-        | Output(resList) -> 
-            if res.Length >= 100
-                then 
-                    res |> printAndSend
-                else
-                    res <- res @ resList
-        | Done(completeMsg) -> 
-            printfn $"[INFO][DONE]: {completeMsg}" 
-            if res.Length > 0 then printAndSend res
-            remoteServer <! Done(completeMsg)
+        | Register(info) -> 
+            printfn $"register info: {info}"
+            // sender <! sprintf "Echo: %s" info
         | _ -> ()
         return! loop()
     }
     loop()
-let postManRef = spawn system "PostMan" postMan
+
+(*/ Print results and send them to server /*)
+let printer (mailbox:Actor<_>) =
+    let mutable res = []
+    let rec loop () = actor {
+        let! message = mailbox.Receive()
+        // printfn "worker acotr receive msg: %A" message
+        let printRes resList = 
+            printfn "-------------RESULT-------------" 
+            resList |> List.iter(fun (str, sha256) -> printfn $"{str}\t{sha256}")
+            res <- []
+            printfn "--------------------------------" 
+        match message with
+        | Output(resList) -> 
+            if res.Length >= 100
+                then 
+                    res |> printRes
+                else
+                    res <- res @ resList
+        | Done(completeMsg) -> 
+            printfn $"[INFO][DONE]: {completeMsg}"
+            if res.Length > 0 then printRes res
+        | _ -> ()
+        return! loop()
+    }
+    loop()
+let printerRef = spawn system "printer" printer
 
 (*/ Worker Actors
     Takes input from remoteActor, calculate results and pass the result to PostMan Actor
@@ -101,21 +107,19 @@ let worker (mailbox:Actor<_>) =
             // printfn "%A %A" res.IsEmpty res
             if res.IsEmpty 
                 then 
-                    outBox <! Done($"[TID: {tid}]\tNotFound")
+                    outBox <! Done($"[TID: {tid}]\tNotFound!")
                 else 
                     outBox <! Output(res)
-                    outBox <! Done($"[TID: {tid}]\tFound\t")
+                    outBox <! Done($"[TID: {tid}]\tFound!\t")
         | _ -> ()
         return! loop()
     }
     loop()
  
 
-let remoteActor (mailbox:Actor<_>) = 
+let localActor (mailbox:Actor<_>) = 
     let actcount = System.Environment.ProcessorCount |> int64
     let totalWorkers = actcount*125L
-    (Async.RunSynchronously (postManRef <? Register($"RemoteStart@{System.Environment.MachineName}"))) |> ignore
-    // postManRef <! Register($"RemoteStart@{System.Environment.MachineName}")
 
     printfn "ProcessorCount: %d" actcount
     printfn "totalWorker: %d" totalWorkers
@@ -124,66 +128,80 @@ let remoteActor (mailbox:Actor<_>) =
             [1L .. totalWorkers]
             |> List.map(fun id -> spawn system (sprintf "Local_%d" id) worker)
 
-    let workerenum = [|for i = 1 to workersPool.Length + 1 do (sprintf "/user/Local_%d" i)|] // (workersPool |> List.mapi(fun id _ -> (sprintf "/user/workers/Local_%d" id)))
+    let workerenum = [|for i = 1 to workersPool.Length + 1 do (sprintf "/user/Local_%d" i)|]
     let workerSystem = system.ActorOf(Props.Empty.WithRouter(Akka.Routing.RoundRobinGroup(workerenum)))
-    let mutable completedWorkerNum = 0L
-    let mutable actorNum = totalWorkers
+    let mutable completedLocalWorkerNum = 0L
+    let mutable completedRemoteWorkerNum = 0L
+    let mutable localActorNum = totalWorkers
+    let mutable remoteActorNum = 0L
     let mutable taskSize = 1E6 |> int64
 
 // Assign tasks to worker
     let rec loop () = actor {
         let! message = mailbox.Receive()
+        let sender = mailbox.Sender()
         // printfn $"Boss received {message}"
         match message with 
         | TaskSize(size) -> taskSize <- size
+        | Register info -> 
+            remoteActorNum <- remoteActorNum + 1L
+            printfn $"[INFO]: Remoter actor No.: {remoteActorNum}"
+            sender <! Register "Acknowledged"
         | Input(n,k,t) -> 
-            // reigster remote acter
-            // postManRef <! Register($"RemoteStart@{System.Environment.MachineName}")
             // task init
             let totalTasks = k - n
-            let taskNum = 
+            let requiredActorNum = 
                 if totalTasks % taskSize = 0L then totalTasks / taskSize else totalTasks / taskSize + 1L
             // printfn "taskNum %d" taskNum
             let assignTasks (size, actors) = 
                 printfn $"[DEBUG]: Task size: {size}"
                 [1L..actors] |> List.iteri(fun i x -> 
-                    printfn $"- Initialize actor [{i + 1}/{actors}]: \t{int64 i * size + n } - {(int64 i + 1L)* size + n - 1L}"
+                    printfn $"- Initialize actor [{i + 1}/{actors}]: \t{int64 i * size + n} - {(int64 i + 1L)* size + n - 1L}"
                     workerSystem <! Input(int64 i * size + n, size, t)
                 )
             // assign tasks based on actor number
-            match taskNum with
-            | _ when taskNum > actorNum ->
+            match requiredActorNum with
+            | _ when requiredActorNum > localActorNum ->
                 // resize taskSize to match actor number
-                if (totalTasks % actorNum = 0L) then taskSize <- totalTasks / actorNum else taskSize <- totalTasks / actorNum + 1L
-                assignTasks(taskSize, actorNum)
-            | _ when taskNum = actorNum -> 
-                assignTasks(taskSize, actorNum)
-            | _ when taskNum < actorNum -> 
+                if (totalTasks % localActorNum = 0L) then taskSize <- totalTasks / localActorNum else taskSize <- totalTasks / localActorNum + 1L
+                assignTasks(taskSize, localActorNum)
+            | _ when requiredActorNum = localActorNum -> 
+                assignTasks(taskSize, localActorNum)
+            | _ when requiredActorNum < localActorNum -> 
                 // reduce actor numbers
-                actorNum <- taskNum
-                // printfn $"totalTasks: {totalTasks}  actorNum: {actorNum}"
-                if totalTasks < taskSize then assignTasks(totalTasks, taskNum) else assignTasks(taskSize, taskNum)
+                localActorNum <- requiredActorNum
+                // printfn $"totalTasks: {totalTasks}  actorNum: {localActorNum}"
+                if totalTasks < taskSize then assignTasks(totalTasks, requiredActorNum) else assignTasks(taskSize, requiredActorNum)
             | _ -> failwith "[ERROR] wrong taskNum"
+            // printfn "End Input"
         | Output (res) -> 
-            postManRef <! Output(res)
+            printerRef <! Output(res)
         | Done(completeMsg) ->
-            completedWorkerNum <- completedWorkerNum + 1L
-            printfn $"> {completeMsg} \tcompleted:{completedWorkerNum} \ttotal:{actorNum}" 
-            if completedWorkerNum = actorNum then
-                postManRef <! Done($"RemoteDone@{System.Environment.MachineName}")
+            let containPrefix (p:string) (s:string) = s.StartsWith(p)
+            if completeMsg |> containPrefix "RemoteDone"
+                then
+                    completedRemoteWorkerNum <- completedRemoteWorkerNum + 1L
+                    printfn $"> {completeMsg} \tcompleted:{completedRemoteWorkerNum} \ttotal:{remoteActorNum}" 
+                else
+                    completedLocalWorkerNum <- completedLocalWorkerNum + 1L
+                    printfn $"> {completeMsg} \tcompleted:{completedLocalWorkerNum} \ttotal:{localActorNum}" 
+            if completedLocalWorkerNum = localActorNum && completedRemoteWorkerNum = remoteActorNum then
+                printerRef <! Done($"All tasks completed! local: {completedLocalWorkerNum}, remote: {completedRemoteWorkerNum}")
                 mailbox.Context.System.Terminate() |> ignore
-        | _ -> ()
-
+        // | _ -> ()
         return! loop()
     }
     loop()
 
-let client = spawn system "remoteActor" remoteActor
+let client = spawn system "localActor" localActor
+// spawn system "localActor" localActor
+// spawn system "receiver" receiver
+// system.ActorOf(Props.Empty.WithRouter(new Akka.Routing.RoundRobinGroup("/user/receive")), "StringDigger");
 // Input from Command Line
 let N = fsi.CommandLineArgs.[1] |> int64
 let K = fsi.CommandLineArgs.[2] |> int64
 let T = fsi.CommandLineArgs.[3] |> int64
-client <! TaskSize(int64 1E5)
+// client <! TaskSize(int64 2.5E6)
 client <! Input(N, K, T)
 // Wait until all the actors has finished processing
 system.WhenTerminated.Wait()
